7c1f838a3ce8cb1116b1c4380cafd47175d79f78
quantrinh123 <quantrinh29092003@gmail.com>
2024-10-20 17:54:02 +0700

    .

---
+++ src/main/java/com/pbl6/music/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.pbl6.music.mapper;
+
+import com.pbl6.music.dto.request.UserRequestDTO;
+import com.pbl6.music.dto.response.UserResponseDTO;
+import com.pbl6.music.entity.UserEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserEntity toUserEntity(UserRequestDTO userRequestDTO);
+
+    UserResponseDTO toUserResponseDTO(UserEntity userEntity);
+
+}
